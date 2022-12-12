798bd34ce9e2289401aebf268b1cf898d8ae3db3
rkekqmf <rkekqmf@gmail.com>
2022-12-13 01:31:25 +0900

    마지막

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 import { data } from "./data";
@@ -5,28 +6,61 @@ import { data } from "./data";
 function App() {
   const [users, setUsers] = useState([]);
 
+  // useEffect(() => {
+  //   for (let i = 0; i < data.length; i++) {
+  //     fetch(`https://developer-lostark.game.onstove.com/armories/characters/${data[i]}/profiles`, {
+  //       method: "get",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Accept: "application/json",
+  //         authorization:
+  //           "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDEwMzAifQ.kJZYMxhNjtRjpekZhaWw-2HbADtm6AUCSNBKBhehxAmn3dy2NP95o6YSvVw2pQSXrMYP3VSrVZUTWQkYvyBaIHEAqVN5W4qotY3S4Roe0DC8mAKLOa1JQNZEbShgYBAYT0Ehj66e7B1wTELoT6ZCYx-nZJhApdqTP0C_wpcvdNlOAxM1nueIlknj4irk_kszbErdhxZAJqBa2unXCDmOInQjxNng-trWXPBhTdCbGPW4hStAHfYQC-X-Ve9A5knSmUpLzITxeRB3gjVcg4lWZNwviyKNGXIF0Gvld9_JbdqTW8zLggdapNTlsKGTpwD5oGlsd3L7dCor5IT-FIiSRg",
+  //       },
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => setUsers((users) => [...users, data]));
+  //   }
+  // }, []);
+
   useEffect(() => {
     for (let i = 0; i < data.length; i++) {
-      fetch(`/armories/characters/${data[i]}/profiles`, {
-        "Access-Control-Allow-Origin": "https://developer-lostark.game.onstove.com",
-        method: "get",
+      axios({
+        method: "get", // 통신 방식
+        url: `/armories/characters/${data[i]}/profiles`,
+        proxy: "https://developer-lostark.game.onstove.com",
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
           authorization:
             "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDEwMzAifQ.kJZYMxhNjtRjpekZhaWw-2HbADtm6AUCSNBKBhehxAmn3dy2NP95o6YSvVw2pQSXrMYP3VSrVZUTWQkYvyBaIHEAqVN5W4qotY3S4Roe0DC8mAKLOa1JQNZEbShgYBAYT0Ehj66e7B1wTELoT6ZCYx-nZJhApdqTP0C_wpcvdNlOAxM1nueIlknj4irk_kszbErdhxZAJqBa2unXCDmOInQjxNng-trWXPBhTdCbGPW4hStAHfYQC-X-Ve9A5knSmUpLzITxeRB3gjVcg4lWZNwviyKNGXIF0Gvld9_JbdqTW8zLggdapNTlsKGTpwD5oGlsd3L7dCor5IT-FIiSRg",
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => setUsers((users) => [...users, data]));
+        }, // 요청 헤더 설정
+        responseType: "json", // default
+      }).then(function (response) {
+        setUsers((users) => [...users, response.data]);
+        console.log("성공");
+      });
     }
   }, []);
-  console.log(users);
+
+  // var xmlHttpRequest = new XMLHttpRequest();
+  // xmlHttpRequest.open(
+  //   "GET",
+  //   "/armories/characters/서씨가문의수치/profiles",
+  //   true
+  // );
+  // xmlHttpRequest.setRequestHeader("accept", "application/json");
+  // xmlHttpRequest.setRequestHeader(
+  //   "authorization",
+  //   "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDEwMzAifQ.kJZYMxhNjtRjpekZhaWw-2HbADtm6AUCSNBKBhehxAmn3dy2NP95o6YSvVw2pQSXrMYP3VSrVZUTWQkYvyBaIHEAqVN5W4qotY3S4Roe0DC8mAKLOa1JQNZEbShgYBAYT0Ehj66e7B1wTELoT6ZCYx-nZJhApdqTP0C_wpcvdNlOAxM1nueIlknj4irk_kszbErdhxZAJqBa2unXCDmOInQjxNng-trWXPBhTdCbGPW4hStAHfYQC-X-Ve9A5knSmUpLzITxeRB3gjVcg4lWZNwviyKNGXIF0Gvld9_JbdqTW8zLggdapNTlsKGTpwD5oGlsd3L7dCor5IT-FIiSRg"
+  // );
+  // xmlHttpRequest.onreadystatechange = () => {};
+  // xmlHttpRequest.send();
+  // console.log(xmlHttpRequest);
   return (
     <div className="App1">
       <h1>필연 단체 군장검사x</h1>
 
-      {users.map((user) => {
+      {users?.map((user) => {
         return (
           <div className="App2">
             <img className="App3" src={user?.CharacterImage} alt="" />
