2a6fdf560e1191b21425d9699dd8f605d8635cef
rkekqmf <rkekqmf@gmail.com>
2022-12-13 01:58:21 +0900

    마지막

--- src/App.js
+++ src/App.js
@@ -26,13 +26,11 @@ function App() {
     for (let i = 0; i < data.length; i++) {
       axios({
         method: "get", // 통신 방식
-        url: `/armories/characters/${data[i]}/profiles`,
-        proxy: "https://developer-lostark.game.onstove.com",
+        url: `${process.env.REACT_APP_DB_HOST}/armories/characters/${data[i]}/profiles`,
         headers: {
           "Content-Type": "application/json",
           Accept: "application/json",
-          authorization:
-            "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyIsImtpZCI6IktYMk40TkRDSTJ5NTA5NWpjTWk5TllqY2lyZyJ9.eyJpc3MiOiJodHRwczovL2x1ZHkuZ2FtZS5vbnN0b3ZlLmNvbSIsImF1ZCI6Imh0dHBzOi8vbHVkeS5nYW1lLm9uc3RvdmUuY29tL3Jlc291cmNlcyIsImNsaWVudF9pZCI6IjEwMDAwMDAwMDAwMDEwMzAifQ.kJZYMxhNjtRjpekZhaWw-2HbADtm6AUCSNBKBhehxAmn3dy2NP95o6YSvVw2pQSXrMYP3VSrVZUTWQkYvyBaIHEAqVN5W4qotY3S4Roe0DC8mAKLOa1JQNZEbShgYBAYT0Ehj66e7B1wTELoT6ZCYx-nZJhApdqTP0C_wpcvdNlOAxM1nueIlknj4irk_kszbErdhxZAJqBa2unXCDmOInQjxNng-trWXPBhTdCbGPW4hStAHfYQC-X-Ve9A5knSmUpLzITxeRB3gjVcg4lWZNwviyKNGXIF0Gvld9_JbdqTW8zLggdapNTlsKGTpwD5oGlsd3L7dCor5IT-FIiSRg",
+          authorization: `bearer ${process.env.REACT_APP_API_KEY}`,
         }, // 요청 헤더 설정
         responseType: "json", // default
       }).then(function (response) {
