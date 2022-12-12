c979fac811396a48a36d3d6f75dff2ee42833aa5
rkekqmf <rkekqmf@gmail.com>
2022-12-12 22:43:54 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import { data } from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    for (let i = 0; i < data.length; i++) {
+      fetch(`/armories/characters/${data[i]}/profiles`, {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `bearer ${process.env.REACT_APP_API_KEY}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => setUsers((users) => [...users, data]));
+    }
+  }, []);
+  console.log(users);
+  return (
+    <div className="App1">
+      <h1>필연 단체 군장검사</h1>
+
+      {users.map((user) => {
+        return (
+          <div className="App2">
+            <img className="App3" src={user?.CharacterImage} alt="" />
+            <div>
+              <p>{user?.GuildMemberGrade}</p>
+              <p>{user?.CharacterClassName}</p>
+              <p>{user?.Title}</p>
+              <p>{user?.CharacterName}</p>
+              <p>{user?.ItemMaxLevel}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
