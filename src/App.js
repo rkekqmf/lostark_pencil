8b4061d2225bdea24e0b21a6916c665a6e8aa048
rkekqmf <rkekqmf@gmail.com>
2022-12-13 12:43:44 +0900

    마지막

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
   useEffect(() => {
     for (let i = 0; i < data.length; i++) {
       axios({
+        withCredentials: false,
         method: "get",
         url: `https://developer-lostark.game.onstove.com/armories/characters/${data[i]}/profiles`,
         headers: {
