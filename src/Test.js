cfff931848b802542cc88d3250d27a68009b2e4f
rkekqmf <rkekqmf@gmail.com>
2022-12-21 00:28:29 +0900

    .

--- src/Test.js
+++ src/Test.js
@@ -1,31 +1,39 @@
+import "./Test.css";
+
 const Test = ({ profile, collection }) => {
   console.log(profile[0]);
   console.log(collection[0]);
   return (
     <div className="App2">
-      <img className="App3" src={profile[0]?.CharacterImage} alt="" />
-      <div>{profile[0]?.CharacterClassName}</div>
-      <div>{profile[0]?.CharacterLevel}</div>
-      <div>{profile[0]?.CharacterName}</div>
-      <div>{profile[0]?.ExpeditionLevel}</div>
-      <div>{profile[0]?.GuildName}</div>
-      <div>{profile[0]?.ItemAvgLevel}</div>
-      <div>{profile[0]?.PvpGradeName}</div>
-      <div>{profile[0]?.ServerName}</div>
-      <div>{profile[0]?.Title}</div>
-      <div>{profile[0]?.TownLevel}</div>
-      <div>{profile[0]?.TownName}</div>
-      {collection[0]?.map((col) => {
-        return (
-          <>
-            <img src={col.Icon} alt="" />
-            <div>{col.Type}</div>
-            <div>
-              {col.Point} / {col.MaxPoint}
+      <div className="App3">
+        <img className="Img1" src={profile[0]?.CharacterImage} alt="" />
+        <div>
+          <p>{profile[0]?.CharacterClassName}</p>
+          <p>{profile[0]?.CharacterLevel}</p>
+          <p>{profile[0]?.CharacterName}</p>
+          <p>{profile[0]?.ExpeditionLevel}</p>
+          <p>{profile[0]?.GuildName}</p>
+          <p>{profile[0]?.ItemAvgLevel}</p>
+          <p>{profile[0]?.PvpGradeName}</p>
+          <p>{profile[0]?.ServerName}</p>
+          <p>{profile[0]?.Title}</p>
+          <p>{profile[0]?.TownLevel}</p>
+          <p>{profile[0]?.TownName}</p>
+        </div>
+      </div>
+      <div className="App4">
+        {collection[0]?.map((col) => {
+          return (
+            <div className="App4">
+              <img className="Img2" src={col.Icon} alt="" />
+              <div>{col.Type}</div>
+              <div>
+                {col.Point} / {col.MaxPoint}
+              </div>
             </div>
-          </>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
